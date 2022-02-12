17532cf2a2ae3325506c0c7940d1894ebe930945
bxlong2001 <AT160234@actvn.edu.vn>
2022-02-12 13:34:50 +0700

    fix bug1

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const outputList = $('.output__list')
 const button = $$('button')
 
 const arrayWord = []
+let arrayWordCopy = []
 
 submitBtn.onclick = () => {
     if(wordInput.value && meanInput.value) {
@@ -19,24 +20,26 @@ submitBtn.onclick = () => {
         }
 
         arrayWord.push(valueInput)
+        arrayWordCopy = [...arrayWord]
     }
-
+    
     wordInput.value = ''
     meanInput.value = ''
 }
 
 checkBtn.onclick = () => {
     const htmls = arrayWord.map((array,index) =>
-        `<li>${index}. ${array.word} : ${array.mean}</li>`
+    `<li>${index}. ${array.word} : ${array.mean}</li>`
     ).join('')
     outputList.innerHTML = htmls
-
+    
     wordInput.value = ''
     meanInput.value = ''
 }
 
-let arrayWordCopy = [...arrayWord]
 randomBtn.onclick = () => {
+    wordInput.value = ''
+    meanInput.value = ''
     randomNumber = Math.floor(Math.random() * arrayWordCopy.length)
 
     const arrayWordRandom = arrayWordCopy.find((array,index) => index == randomNumber)
@@ -44,18 +47,15 @@ randomBtn.onclick = () => {
     if(arrayWordCopy.length != 0){
         outputList.innerHTML = `<li class='word__random'>${arrayWordRandom.word}</li>`
         arrayWordCopy.splice(randomNumber, 1)
+        const wordRandom = $('.word__random')
+        wordRandom.onclick = () => {
+            outputList.innerHTML = `<li class='word__random'>${arrayWordRandom.mean}</li>`
+        }
     }else {
         outputList.innerHTML = `<li class='word__random'>Done!</li>`
         arrayWordCopy = [...arrayWord]
     }
-    const wordRandom = $('.word__random')
-    
-    wordRandom.onclick = () => {
-        outputList.innerHTML = `<li class='word__random'>${arrayWordRandom.mean}</li>`
-    }
 
-    wordInput.value = ''
-    meanInput.value = ''
 }
 
 
