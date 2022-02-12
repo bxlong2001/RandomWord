23df6c04f58a6fb7b9db64c880fe4613428859e4
bxlong2001 <AT160234@actvn.edu.vn>
2022-02-12 14:04:43 +0700

    update code2

--- index.js
+++ index.js
@@ -12,9 +12,19 @@ const button = $$('button')
 const arrayWord = []
 let arrayWordCopy = []
 
-function resetInput () {
+const resetInput = () => {
     wordInput.value = ''
     meanInput.value = ''
+}
+
+const checkArray = () => {
+    const arrayWordLength = arrayWord.length
+    if (arrayWordLength >= 0) {
+        checkBtn.removeAttribute('disabled')
+        if (arrayWordLength >= 2) {
+            randomBtn.removeAttribute('disabled')
+        }
+    }
 }
 
 submitBtn.onclick = () => {
@@ -26,10 +36,13 @@ submitBtn.onclick = () => {
         
         arrayWord.push(valueInput)
         arrayWordCopy = [...arrayWord]
+
+        checkArray()
     }
     
     resetInput()
 }
+
 
 checkBtn.onclick = () => {
     resetInput()
@@ -45,9 +58,7 @@ randomBtn.onclick = () => {
     resetInput()
 
     randomNumber = Math.floor(Math.random() * arrayWordCopy.length)
-
     const arrayWordRandom = arrayWordCopy.find((array,index) => index == randomNumber)
-
     if(arrayWordCopy.length != 0){
         outputList.innerHTML = `<li class='word__random'>${arrayWordRandom.word}</li>`
         arrayWordCopy.splice(randomNumber, 1)
